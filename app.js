09afe7ddb92c5e02a12d7fb18a1304463161d93f
[Ethan Jarrell] <[ethan.jarrell@gmail.com]>
2018-02-09 14:45:05 -0500

    started project

---
+++ app.js
@@ -0,0 +1,74 @@
+//====LIST DEPENDENCIES===//
+const express = require('express');
+const parseurl = require('parseurl');
+const bodyParser = require('body-parser');
+const path = require('path');
+const expressValidator = require('express-validator');
+const mongoose = require('mongoose');
+const mustacheExpress = require('mustache-express');
+const session = require('express-session');
+const app = express();
+const url = 'mongodb://EthanJarrell:EJ3102nl1@ds243085.mlab.com:43085/ohanayelp';
+//=========================//
+
+//====SET APP ENGINE===//
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', './views');
+app.use(express.static('public'));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(expressValidator());
+
+//=========================//
+
+//====START SESSION===//
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+//==========================//
+
+//====MONGOOSE PROMISE===//
+
+mongoose.Promise = require('bluebird');
+
+//==========================//
+
+//====MONGOOSE CONNECTION===//
+
+mongoose.connect(url, function (err, db) {
+ if (err) {
+   console.log('Unable to connect to the mongoDB server. Error:', err);
+ } else {
+   console.log('Connection established to', url);
+ }
+});
+
+//==========================//
+
+//====TEST THE CONNECTION/ROOT DIR===//
+
+app.use(function(req, res, next) {
+  console.log('Programming is fun!');
+  next();
+})
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+//==========================//
+
+//====APP LISTEN ON ENVIRONMENT PORT===//
+
+app.listen(process.env.PORT || 3000);
+console.log('starting applicaiton.  Good job!');
+
+//==========================//
